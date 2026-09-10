f1e45a64353f78ed38ff1425fb70a0d529a965aa
Connor Finch <49081590+Connduit@users.noreply.github.com>
2026-09-10 09:00:50 -0400

    Update Toaster.h

--- Toaster.h
+++ Toaster.h
@@ -2,20 +2,22 @@
 #define TOASTER_H
 
 #include "ToasterSubsystem.h"
+#include "Config.h"
 
 class Toaster
 {
 public:
-    Toaster();
+    Toaster(); 
     ~Toaster();
     void createToasterSubsystem();
+    // void initToasterSubsystem(); wrapper function to explicitly call rtlsdr_read_async?
     void startToasterSubsystem();
     void stopToasterSubsystem();
 private:
-    static void signalHandler(int sig); // ✅ Static signal handler
-    static Toaster *instance_;          // ✅ Static pointer to self
+    static void signalHandler(int sig); // Static signal handler
+    static Toaster *instance_;          // Static pointer to self
     ToasterSubsystem *toasterSubsystem_;
-    // Config config_; // TODO:
+    Config config_; // TODO:
 };
 
 
